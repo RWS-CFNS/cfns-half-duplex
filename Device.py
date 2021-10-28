b1bd84513987d0e61d551ba6cc8cdd0ac2936310
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2021-10-28 11:27:31 +0200

    convert all from DOS to Unix line-endings; add LICENSE and credit where due

--- Device.py
+++ Device.py
@@ -1,4 +1,24 @@
-#!/usr/bin/python
+#
+#    CFNS - Rijkswaterstaat CIV, Delft © 2020 - 2021 <cfns@rws.nl>
+#
+#    Copyright 2020 - 2021 Alfred Espinosa Encarnación <alfred.espinosaencarnacion@rws.nl>
+#
+#    This file is part of cfns-half-duplex
+#
+#    cfns-half-duplex is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    cfns-half-duplex is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with cfns-half-duplex. If not, see <https://www.gnu.org/licenses/>.
+#
+
 from Interface.Interface import Interface
 
 
