bc3b4ee6918efcdbc217db361789959e80630215
FrankMonteny8 <frankmonteny02@gmail.com>
2021-12-27 15:06:35 +0100

    added comments to the client_template. Interface is now version 1.0

--- client_interface/client_template.py
+++ client_interface/client_template.py
@@ -8,17 +8,22 @@ def pad_msg_length(padding_size, message_length):
     message_length += b' ' * (padding_size - len(message_length))
     return message_length
 
+# Connect to interface
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("192.168.178.68", 8001))
 
+# Build message
 message = json.dumps({"request_type": "latest", "valid": False}).encode()
 message_length = pad_msg_length(max_msg_length, len(message))
 
+# Send message 
 client.send(message_length)
 client.send(message)
 
+# Receive reply
 reply_length = int(client.recv(max_msg_length).decode())
 reply = client.recv(reply_length).decode()
 
+# Extract reply
 reply_dict = json.loads(reply)
 print(reply)
